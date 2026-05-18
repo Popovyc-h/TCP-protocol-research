9526167e34ec32095e94f612faeffce9c63a23b0
Popovyc-h <popovichtanja86@gmail.com>
2026-05-18 21:08:07 +0300

    Update Program.cs

--- Simple Socket Client/Program.cs
+++ Simple Socket Client/Program.cs
@@ -22,18 +22,18 @@ class SimpleSocketClient
             5000
             );
 
-        Console.WriteLine($"Підключення до сервера: {clientSocket.RemoteEndPoint}");
-
         // 3. З'єднатися з сервером (Connect)
         clientSocket.Connect(endpoint);
+        Console.WriteLine($"Підключення до сервера: {clientSocket.RemoteEndPoint}");
         Console.WriteLine("Підключено!");
 
         // 4. Підготувати повідомлення та конвертувати в байти
-        string message = "HI!";
+        string message = "Привіт, сервер!";
         var sendBuffer = Encoding.UTF8.GetBytes(message);
 
         // 5. Відправити дані (Send)
         clientSocket.Send(sendBuffer);
+        Console.WriteLine($"Відправлено: {message}");
 
         // 6. Отримати відповідь у буфер (Receive)
         byte[] receiveBuffer = new byte[1024];
@@ -41,10 +41,11 @@ class SimpleSocketClient
 
         // 7. Декодувати відповідь з байтів у рядок
         string receivedStr = Encoding.UTF8.GetString(receiveBuffer, 0, received);
-        Console.WriteLine($"Отримано відповідь: Echo: {receivedStr}");
+        Console.WriteLine($"Отримано відповідь: {receivedStr}");
 
         // 8. Закрити з'єднання (Shutdown, Close)
         clientSocket.Shutdown(SocketShutdown.Both);
         clientSocket.Close();
+        Console.WriteLine("З'єднання закрито");
     }
 }
